252111f253aa8529f7bf7dd3f79d85844f6a7f8c
pangulan <angulopaula12345@gmail.com>
2024-10-24 02:14:30 -0500

    Initial commit

---
+++ src/main/java/com/gestion/gestion_proyectos/GestionProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.gestion_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/gestion_proyectos/GestionProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.gestion_proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
